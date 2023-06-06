ff5e54185d06e241d1340b83002f40362f5788f0
R23-design <134691060+R23-design@users.noreply.github.com>
2023-06-06 18:32:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,12 @@ $(function(){
     return false;
   });
 });
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+})
